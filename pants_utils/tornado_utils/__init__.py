5007f18a5318d94b50edd09225d8ab78b8389527
Rob McQueen <robmcqn@gmail.com>
2014-06-19 14:58:07 -0700

    Add relative paths for __init__ imports

--- pants_utils/tornado_utils/__init__.py
+++ pants_utils/tornado_utils/__init__.py
@@ -2,5 +2,5 @@
 # Tornado Utilities that help with PEX deploys via Pants
 #
 
-from static_handler import PexStaticFileHandler
-from loaders import PexTemplateLoader
+from .static_handler import PexStaticFileHandler
+from .loaders import PexTemplateLoader
